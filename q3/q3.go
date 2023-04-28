45c0a9e2c317b95ff096fd52fcadc898cd8d6ee2
Breno Teixeira Marques <breno.marques@dinfor.uniceub>
2023-04-28 10:58:12 -0300

    f

--- q3/q3.go
+++ q3/q3.go
@@ -15,7 +15,9 @@ import "fmt"
 //Se M ou N forem iguais ou menores que 0, a função deve retornar um erro.
 
 func DominoPieces(m, n int) (int, error) {
-	if m <= 0 && n <= 0 {
+	if m <= 0 {
+		return 0, fmt.Errorf("impossível")
+	} else if n <= 0 {
 		return 0, fmt.Errorf("impossível")
 	}
 
